9945a8967a0f34a459b6893cc8fe196ce365b46d
JoaoBianco <pepe.bianco@hotmail.com>
2023-02-27 15:36:00 -0300

    Generics

---
+++ 03purets/dist/myGenerics.js
@@ -0,0 +1,16 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "t", type: 2 });
