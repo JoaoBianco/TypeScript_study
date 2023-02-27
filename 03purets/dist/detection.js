45c6cc0802750a5d3f8c1ab700c3dbe019d9050b
JoaoBianco <pepe.bianco@hotmail.com>
2023-02-27 16:33:24 -0300

    End TS tutorial

--- 03purets/dist/detection.js
+++ 03purets/dist/detection.js
@@ -16,3 +16,45 @@ function isAdminAccount(account) {
         return account.isAdmin;
     }
 }
+//Instanceof operator
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swin !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    //   return shape.side * shape.side;
+}
+//Exhaustive check
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default: // If is a new type, this will throw an error
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -12,6 +12,8 @@ function provideId(id: string | null) {
   }
 }
 
+//In operator
+
 interface User {
   name: string;
   email: string;
@@ -26,5 +28,77 @@ interface Admin {
 function isAdminAccount(account: User | Admin) {
   if ("isAdmin" in account) {
     return account.isAdmin;
+  }
+}
+
+//Instanceof operator
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+//Type predicate
+
+type Fish = { swin: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swin !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+//Discraminated union
+
+interface Circle {
+  kind: "circle"; //   <-- Discraminated union (define a kind(?))
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  //   return shape.side * shape.side;
+}
+
+//Exhaustive check
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default: // If is a new type, this will throw an error
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
   }
 }
