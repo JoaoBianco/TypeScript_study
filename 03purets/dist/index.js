4ca8f23e690f228b7927b1aeda3296e1d8749b52
JoaoBianco <pepe.bianco@hotmail.com>
2023-02-27 14:45:13 -0300

    Get and set

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,8 +1,32 @@
 "use strict";
+// class User {
+//   public email: string;
+//   name: string;
+//   private readonly city: string = "Jabuka";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+//This is the same as the constructor
 class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._couseCount = 1;
+        this.city = "Jabuka";
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get getCouseCount() {
+        return this._couseCount;
+    }
+    set setCouseCount(courseNumber) {
+        this._couseCount = courseNumber;
     }
 }
 const hi = new User("email@example.com", "example");
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,9 +1,34 @@
+// class User {
+//   public email: string;
+//   name: string;
+//   private readonly city: string = "Jabuka";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+//This is the same as the constructor
 class User {
-  email: string;
-  name: string;
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  private _couseCount = 1;
+
+  private readonly city: string = "Jabuka";
+  constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get getCouseCount(): number {
+    return this._couseCount;
+  }
+
+  set setCouseCount(courseNumber: number) {
+    this._couseCount = courseNumber;
   }
 }
 
